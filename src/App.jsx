dbb661ec54fce0891bab4a598adf17fd672dcde6
Vincent <Vincentnguyen030@gmail.com>
2023-12-20 14:26:32 -0600

    Add useState hook to general info

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,35 @@ import GenInfo from "./GenInfo";
 
 export default function App({nameVal,emailVal,phoneVal}) {
   const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [phoneNum,setPhoneNum] = useState('');
   return (
     <div>
       <label>
+        Name:{" "}
         <input 
           type="text"
           value={name}
           onChange={(event)=>setName(event.target.value)}
         />
       </label>
-      <GenInfo nameVal={nameVal} emailVal={emailVal} phoneVal={phoneVal} />
+      <label>
+        Email:{" "}
+        <input 
+          type="text"
+          value={email}
+          onChange={(event)=>setEmail(event.target.value)}
+        />
+      </label>
+      <label>
+        Phone Number:{" "}
+        <input 
+          type="text"
+          value={phoneNum}
+          onChange={(event)=>setPhoneNum(event.target.value)}
+        />
+      </label>
+      <GenInfo nameVal={name} emailVal={email} phoneVal={phoneNum} />
     </div>
   );
 }

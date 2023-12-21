1d20f35dd94cd56bcf82f0cd180d28cab0c67e37
Vincent <Vincentnguyen030@gmail.com>
2023-12-20 23:44:48 -0600

    Add arrays to hold data to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,32 @@
 import { useState } from "react";
 import GenInfo from "./GenInfo";
-import {ExpForm,InfoForm} from "./Form";
+import {GenInfoForm,ExpForm,EducationForm} from "./Form";
 
 export default function App() {
   const [name,setName] = useState('');
   const [email,setEmail] = useState('');
   const [phoneNum,setPhoneNum] = useState('');
-  const [school,setSchool] = useState('');
-  const [major,setMajor] = useState('');
-  const [educationDate,setEducationDate] = useState('');
   const [toggle,setToggle] = useState(false);
-  const [companyName,setCompanyName] = useState('');
-  const [position,setPosition] = useState('');
-  const [responsibilities,setResponsibilities] = useState('');
-  const [startDate,setStartDate] = useState('');
-  const [endDate,setEndDate] = useState('');
 
-  const toggleButton = () => {
-    console.log(toggle);
-    setToggle(!toggle);
-  }
+  const experienceList = [];
+  const educationList = [];
 
   return (
     <div>
       <div className="userinput">
-        <InfoForm 
-          formName="genInfo"
-          val1name="Name:"
-          val2name="Email:"
-          val3name="Phone Number:"
-          val1={name}
-          val2={email}
-          val3={phoneNum}
-          setVal1={setName}
-          setVal2={setEmail}
-          setVal3={setPhoneNum}
+        <GenInfoForm 
+          formName="genInfo" val1name="Name:" val2name="Email:" val3name="Phone Number:" val1={name} val2={email} val3={phoneNum} setVal1={setName} setVal2={setEmail} setVal3={setPhoneNum}
         />
-        <InfoForm 
-          formName="education"
-          val1name="School Name:"
-          val2name="Field Of Study:"
-          val3name="Date of Study:"
-          val1={school}
-          val2={major}
-          val3={educationDate}
-          setVal1={setSchool}
-          setVal2={setMajor}
-          setVal3={setEducationDate}
-        />
+        <EducationForm array={educationList}/>
         <div className="experience">
-          {/* need to add a button that checks if active and creates experience form if not */}
-          <ExpForm 
-            formName="experienceForm"
-            val1name="Company Name:"
-            val2name="Position Title:"
-            val3name="Responsibilities:"
-            val4name="Date Started:"
-            val5name="Date Ended:"
-            val1={companyName}
-            val2={position}
-            val3={responsibilities}
-            val4={startDate}
-            val5={endDate}
-            setVal1={setCompanyName}
-            setVal2={setPosition}
-            setVal3={setResponsibilities}
-            setVal4={setStartDate}
-            setVal5={setEndDate}
-            toggled={toggle}
-          />
-          <button onClick={toggleButton}>Add Experience</button>
+          {/* 
+          need to add a button that checks if active and creates experience form if not
+          create collapsable education and experience
+          create array of objects to hold entered information
+          possibly implement state that holds objects of information entered, only allow one form within education/experience to be shown at a time, this returns index which will access the array and pressing save will edit that index's object
+          */}
+          <ExpForm array={experienceList}/>
+          <button onClick={()=>setToggle(!toggle)}>Add Experience</button>
         </div>
       </div>
       <div className="result">
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function Field({label,val,setVal}) {
     return (
         <label>
@@ -11,7 +13,7 @@ function Field({label,val,setVal}) {
       );
 }
 
-function Form({
+function GenInfoForm({
     formName,
     val1name,
     val2name,
@@ -32,36 +34,41 @@ function Form({
     );
 }
 
-function ExperienceForm({
-    formName,
-    val1name,
-    val2name,
-    val3name,
-    val4name,
-    val5name,
-    val1,
-    val2,
-    val3,
-    val4,
-    val5,
-    setVal1,
-    setVal2,
-    setVal3,
-    setVal4,
-    setVal5,
-    toggled
-    }) {
-    return toggled 
-        ?
-        <div className={formName}>
-            <Field label={val1name} val={val1} setVal={setVal1}/>
-            <Field label={val2name} val={val2} setVal={setVal2}/>
-            <Field label={val3name} val={val3} setVal={setVal3}/>
-            <Field label={val4name} val={val4} setVal={setVal4}/>
-            <Field label={val5name} val={val5} setVal={setVal5}/>
+function EducationForm({array}) {
+    const [school,setSchool] = useState('');
+    const [major,setMajor] = useState('');
+    const [educationDate,setEducationDate] = useState('');
+
+    return (
+        <div className="genInfo">
+            <Field label="School Name:" val={school} setVal={setSchool}/>
+            <Field label="Field of Study:" val={major} setVal={setMajor}/>
+            <Field label="Date of Study:" val={educationDate} setVal={setEducationDate}/>
         </div>
-        :
-        null;
+    );
 }
 
-export {Form as InfoForm, ExperienceForm as ExpForm};
+function ExperienceForm({array}) {
+        const [companyName,setCompanyName] = useState('');
+        const [position,setPosition] = useState('');
+        const [responsibilities,setResponsibilities] = useState('');
+        const [startDate,setStartDate] = useState('');
+        const [endDate,setEndDate] = useState('');
+        const [show,setShow] = useState(true);
+
+        return show
+            ?
+            <div className="expForm">
+                <Field label="Company Name:" val={companyName} setVal={setCompanyName}/>
+                <Field label="Position Title:" val={position} setVal={setPosition}/>
+                <Field label="Responsibilities:" val={responsibilities} setVal={setResponsibilities}/>
+                <Field label="Date Started" val={startDate} setVal={setStartDate}/>
+                <Field label="Date Ended" val={endDate} setVal={setEndDate}/>
+                <button onClick={()=>setShow(!show)}>Show</button>
+                <button>Save</button>
+            </div>
+            :
+            null;
+}
+
+export {GenInfoForm as GenInfoForm, ExperienceForm as ExpForm, EducationForm as EducationForm};
